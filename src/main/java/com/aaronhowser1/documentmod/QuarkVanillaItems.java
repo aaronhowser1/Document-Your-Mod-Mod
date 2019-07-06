380a224df57cb8625dd237bbb51b13f29dadacca
Aaron Howser <aaronhowser1@gmail.com>
2019-07-05 19:53:40 -0500

    Made Shulker Box info way smaller using arrays

--- src/main/java/com/aaronhowser1/documentmod/QuarkVanillaItems.java
+++ src/main/java/com/aaronhowser1/documentmod/QuarkVanillaItems.java
@@ -1,5 +1,6 @@
 package com.aaronhowser1.documentmod;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import static com.aaronhowser1.documentmod.DocumentModJEIIntegration.*;
@@ -22,54 +23,15 @@ public class QuarkVanillaItems
         addItemInfo(JUNGLE_BOAT, "documentationmod.quark.vanilla.boat");
         addItemInfo(ACACIA_BOAT, "documentationmod.quark.vanilla.boat");
         addItemInfo(DARK_OAK_BOAT, "documentationmod.quark.vanilla.boat");
-        addBlockInfo(WHITE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(ORANGE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(MAGENTA_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(LIGHT_BLUE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(YELLOW_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(LIME_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(PINK_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(GRAY_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(SILVER_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(CYAN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(PURPLE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(BLUE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(BROWN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(GREEN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(RED_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(BLACK_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerexplosion");
-        addBlockInfo(WHITE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(ORANGE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(MAGENTA_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(LIGHT_BLUE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(YELLOW_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(LIME_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(PINK_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(GRAY_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(SILVER_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(CYAN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(PURPLE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(BLUE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(BROWN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(GREEN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(RED_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(BLACK_SHULKER_BOX, "documentationmod.quark.vanilla.shulkerhover");
-        addBlockInfo(WHITE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(ORANGE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(MAGENTA_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(LIGHT_BLUE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(YELLOW_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(LIME_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(PINK_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(GRAY_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(SILVER_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(CYAN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(PURPLE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(BLUE_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(BROWN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(GREEN_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(RED_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
-        addBlockInfo(BLACK_SHULKER_BOX, "documentationmod.quark.vanilla.shulkeradd");
+
+        Block[] shulkerBoxes = {WHITE_SHULKER_BOX, ORANGE_SHULKER_BOX, MAGENTA_SHULKER_BOX, LIGHT_BLUE_SHULKER_BOX, YELLOW_SHULKER_BOX, LIME_SHULKER_BOX, PINK_SHULKER_BOX, GRAY_SHULKER_BOX, SILVER_SHULKER_BOX, CYAN_SHULKER_BOX, PURPLE_SHULKER_BOX, BLUE_SHULKER_BOX, BROWN_SHULKER_BOX, GREEN_SHULKER_BOX, RED_SHULKER_BOX, BLACK_SHULKER_BOX};
+        for (Block b: shulkerBoxes)
+        {
+            addBlockInfo(b, "documentationmod.quark.vanilla.shulkerexplosion");
+            addBlockInfo(b, "documentationmod.quark.vanilla.shulkerhover");
+            addBlockInfo(b, "documentationmod.quark.vanilla.shulkeradd");
+        }
+
         addItemInfo(COMPASS, "documentationmod.quark.vanilla.compass");
         addBlockInfo(LADDER, "documentationmod.quark.vanilla.ladder");
         addItemInfo(ARMOR_STAND, "documentationmod.quark.vanilla.armorstand");
