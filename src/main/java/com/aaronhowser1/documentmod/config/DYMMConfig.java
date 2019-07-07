a173e4dabbe0e7ed420e173c77384e7e14fb8b13
Aaron Howser <aaronhowser1@gmail.com>
2019-07-06 19:13:17 -0500

    Changed config to use Subcategories Added config options for each vanilla item changed by Quark Changed boats to use an array instead of adding info to each boat

--- src/main/java/com/aaronhowser1/documentmod/config/DYMMConfig.java
+++ src/main/java/com/aaronhowser1/documentmod/config/DYMMConfig.java
@@ -7,8 +7,53 @@ import net.minecraftforge.common.config.Config;
 public class DYMMConfig {
 
     @Config.Comment("Add information tabs to items added by Quark?")
+    @Config.Name("Quark Info")
     public static boolean useQuark = true;
 
+    @Config.Name("Quark's Vanilla Info")
     @Config.Comment("Add information tabs to vanilla items that Quark changes?")
     public static boolean useVanillaQuark = true;
+
+    @Config.Comment("Toggle each vanilla item's info tab")
+    @Config.Name("Vanilla Items Changed By Quark")
+    public static SubCategory vanillaitemschangedbyquark = new SubCategory();
+
+    public static class SubCategory {
+        @Config.Comment("Say that Dispensers can place blocks etc?")
+        public boolean infoDispenser = true;
+        @Config.Comment("Say that Sticky Pistons can move Tile Entities?")
+        public boolean infoStickyPiston = true;
+        @Config.Comment("Say that Leads can connect 2 Fences?")
+        public boolean infoLead = true;
+        @Config.Comment("Say that Boats can have Chests?")
+        public boolean infoBoat = true;
+        @Config.Comment("Say that Shulker Boxes don't explode?")
+        public boolean infoShulkerBoxExplosion = true;
+        @Config.Comment("Say that Shulker Boxes show their contents when hovered over?")
+        public boolean infoShulkerBoxHover = true;
+        @Config.Comment("Say that you can add items to Shulker Boxes without placing them?")
+        public boolean infoShulkerBoxAdd = true;
+        @Config.Comment("Say that Compasses in other dimensions point towards the portal?")
+        public boolean infoCompass = true;
+        @Config.Comment("Say that you can slide down Ladders and place them from the top?")
+        public boolean infoLadder = true;
+        @Config.Comment("Say that you can swap Armor Stand armor by sneak right clicking?")
+        public boolean infoArmorStand = true;
+        @Config.Comment("Say that Elytras can be dyed?")
+        public boolean infoElytra = true;
+        @Config.Comment("Say that you can edit Signs?")
+        public boolean infoSign = true;
+        @Config.Comment("Say that Villagers follow Emerald Blocks?")
+        public boolean infoEmeraldBlock = true;
+        @Config.Comment("Say that you can place Glowstone Dust?")
+        public boolean infoGlowstoneDust = true;
+        @Config.Comment("Say that you can place Gunpowder?")
+        public boolean infoGunpowder = true;
+        @Config.Comment("Say that you can throw Dragon Breath?")
+        public boolean infoDragonBreath = true;
+        @Config.Comment("Say that Maps show their contents when hovered over?")
+        public boolean infoMap = true;
+        @Config.Comment("Say that Enchanted Books show what tools they can go on?")
+        public boolean infoEnchantedBook = true;
+    }
 }
--- src/main/java/com/aaronhowser1/documentmod/quark/QuarkVanillaItems.java
+++ src/main/java/com/aaronhowser1/documentmod/quark/QuarkVanillaItems.java
@@ -1,9 +1,11 @@
 package com.aaronhowser1.documentmod.quark;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import static com.aaronhowser1.documentmod.DocumentModJEIIntegration.*;
+import static com.aaronhowser1.documentmod.config.DYMMConfig.*;
 import static net.minecraft.init.Blocks.*;
 import static net.minecraft.init.Items.*;
 
@@ -12,38 +14,37 @@ public class QuarkVanillaItems
 {
     public static void init()
     {
-        //TODO Check the Quark config and only put descriptions on items that are enabled
-        addBlockInfo(DISPENSER, "documentationmod.quark.vanilla.dispenser");
-        addBlockInfo(STICKY_PISTON, "documentationmod.quark.vanilla.stickypiston");
-        addItemInfo(LEAD, "documentationmod.quark.vanilla.lead");
-        addItemInfo(BOAT, "documentationmod.quark.vanilla.boat");
-        addItemInfo(BOAT, "documentationmod.quark.vanilla.boat");
-        addItemInfo(SPRUCE_BOAT, "documentationmod.quark.vanilla.boat");
-        addItemInfo(BIRCH_BOAT, "documentationmod.quark.vanilla.boat");
-        addItemInfo(JUNGLE_BOAT, "documentationmod.quark.vanilla.boat");
-        addItemInfo(ACACIA_BOAT, "documentationmod.quark.vanilla.boat");
-        addItemInfo(DARK_OAK_BOAT, "documentationmod.quark.vanilla.boat");
+//        //TODO Check the Quark config and only put descriptions on items that are enabled
+        if(vanillaitemschangedbyquark.infoDispenser) addBlockInfo(DISPENSER, "documentationmod.quark.vanilla.dispenser");
+        if(vanillaitemschangedbyquark.infoStickyPiston) addBlockInfo(STICKY_PISTON, "documentationmod.quark.vanilla.stickypiston");
+        if(vanillaitemschangedbyquark.infoLead) addItemInfo(LEAD, "documentationmod.quark.vanilla.lead");
+
+        Item[] boats = {BOAT, SPRUCE_BOAT, BIRCH_BOAT, JUNGLE_BOAT, ACACIA_BOAT, DARK_OAK_BOAT};
+        for (Item i: boats)
+        {
+            if(vanillaitemschangedbyquark.infoBoat) addItemInfo(i, "documentationmod.quark.vanilla.boat");
+        }
 
         Block[] shulkerBoxes = {WHITE_SHULKER_BOX, ORANGE_SHULKER_BOX, MAGENTA_SHULKER_BOX, LIGHT_BLUE_SHULKER_BOX, YELLOW_SHULKER_BOX, LIME_SHULKER_BOX, PINK_SHULKER_BOX, GRAY_SHULKER_BOX, SILVER_SHULKER_BOX, CYAN_SHULKER_BOX, PURPLE_SHULKER_BOX, BLUE_SHULKER_BOX, BROWN_SHULKER_BOX, GREEN_SHULKER_BOX, RED_SHULKER_BOX, BLACK_SHULKER_BOX};
         for (Block b: shulkerBoxes)
         {
-            addBlockInfo(b, "documentationmod.quark.vanilla.shulkerexplosion");
-            addBlockInfo(b, "documentationmod.quark.vanilla.shulkerhover");
-            addBlockInfo(b, "documentationmod.quark.vanilla.shulkeradd");
+            if(vanillaitemschangedbyquark.infoShulkerBoxExplosion) addBlockInfo(b, "documentationmod.quark.vanilla.shulkerexplosion");
+            if(vanillaitemschangedbyquark.infoShulkerBoxHover) addBlockInfo(b, "documentationmod.quark.vanilla.shulkerhover");
+            if(vanillaitemschangedbyquark.infoShulkerBoxAdd) addBlockInfo(b, "documentationmod.quark.vanilla.shulkeradd");
         }
 
-        addItemInfo(COMPASS, "documentationmod.quark.vanilla.compass");
-        addBlockInfo(LADDER, "documentationmod.quark.vanilla.ladder");
-        addItemInfo(ARMOR_STAND, "documentationmod.quark.vanilla.armorstand");
-        addItemInfo(SIGN, "documentationmod.quark.vanilla.sign");
-        addBlockInfo(EMERALD_BLOCK, "documentationmod.quark.vanilla.emeraldblock");
-        addItemInfo(ELYTRA, "documentationmod.quark.vanilla.elytra");
-        addItemInfo(GLOWSTONE_DUST, "documentationmod.quark.vanilla.glowstonedust");
-        addItemInfo(GUNPOWDER, "documentationmod.quark.vanilla.gunpowder");
-        addItemInfo(DRAGON_BREATH, "documentationmod.quark.vanilla.dragonbreath");
-        addItemInfo(MAP, "documentationmod.quark.vanilla.map");
-        addItemInfo(FILLED_MAP, "documentationmod.quark.vanilla.map");
+        if(vanillaitemschangedbyquark.infoCompass) addItemInfo(COMPASS, "documentationmod.quark.vanilla.compass");
+        if(vanillaitemschangedbyquark.infoLadder) addBlockInfo(LADDER, "documentationmod.quark.vanilla.ladder");
+        if(vanillaitemschangedbyquark.infoArmorStand) addItemInfo(ARMOR_STAND, "documentationmod.quark.vanilla.armorstand");
+        if(vanillaitemschangedbyquark.infoSign) addItemInfo(SIGN, "documentationmod.quark.vanilla.sign");
+        if(vanillaitemschangedbyquark.infoEmeraldBlock) addBlockInfo(EMERALD_BLOCK, "documentationmod.quark.vanilla.emeraldblock");
+        if(vanillaitemschangedbyquark.infoElytra) addItemInfo(ELYTRA, "documentationmod.quark.vanilla.elytra");
+        if(vanillaitemschangedbyquark.infoGlowstoneDust) addItemInfo(GLOWSTONE_DUST, "documentationmod.quark.vanilla.glowstonedust");
+        if(vanillaitemschangedbyquark.infoGunpowder) addItemInfo(GUNPOWDER, "documentationmod.quark.vanilla.gunpowder");
+        if(vanillaitemschangedbyquark.infoDragonBreath) addItemInfo(DRAGON_BREATH, "documentationmod.quark.vanilla.dragonbreath");
+        if(vanillaitemschangedbyquark.infoMap) addItemInfo(MAP, "documentationmod.quark.vanilla.map");
+        if(vanillaitemschangedbyquark.infoMap) addItemInfo(FILLED_MAP, "documentationmod.quark.vanilla.map");
         //TODO figure out why the enchanted book doesn't work
-        addItemInfo(ENCHANTED_BOOK, "documentationmod.quark.vanilla.enchantedbook");
+        if(vanillaitemschangedbyquark.infoEnchantedBook) addItemInfo(ENCHANTED_BOOK, "documentationmod.quark.vanilla.enchantedbook");
     }
 }
