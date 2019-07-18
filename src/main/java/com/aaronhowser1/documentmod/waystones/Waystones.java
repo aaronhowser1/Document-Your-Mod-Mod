a586da172389edf0e8d76d9731ae607e2e6c4a75
Aaron Howser <aaronhowser1@gmail.com>
2019-07-18 11:32:38 -0500

    Waystones documentation actually works now

--- src/main/java/com/aaronhowser1/documentmod/waystones/Waystones.java
+++ src/main/java/com/aaronhowser1/documentmod/waystones/Waystones.java
@@ -5,8 +5,9 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import static com.aaronhowser1.documentmod.DocumentModJEIIntegration.addItemInfo;
+import static com.aaronhowser1.documentmod.config.DYMMConfig.debugModIsDocumented;
 
-@GameRegistry.ObjectHolder("waystone")
+@GameRegistry.ObjectHolder("waystones")
 public class Waystones {
     public static final Item WAYSTONE = Items.AIR;
     public static final Item RETURN_SCROLL = Items.AIR;
@@ -20,5 +21,7 @@ public class Waystones {
         addItemInfo(BOUND_SCROLL, "documentationmod.waystone.bound");
         addItemInfo(WARP_SCROLL, "documentationmod.waystone.warpscroll");
         addItemInfo(WARP_STONE, "documentationmod.waystone.warpstone");
+
+        if(debugModIsDocumented) System.out.println("Waystones is documented");
     }
 }
