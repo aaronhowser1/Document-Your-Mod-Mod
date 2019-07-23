e660b540be600d955a82616fe8c6905e2a3a01ed
TheSilkMiner <thesilkminer@outlook.com>
2019-07-23 12:42:29 +0200

    Add possibility to know what doesn't have documentation
    This only applies to mods that have at least one documented item or
    block and logs all the items that do not have a registered
    `ModDocumentation` entry.
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/aaronhowser1/documentmod/DocumentMod.java
+++ src/main/java/com/aaronhowser1/documentmod/DocumentMod.java
@@ -1,16 +1,26 @@
 package com.aaronhowser1.documentmod;
 
+import com.aaronhowser1.documentmod.config.DYMMConfig;
 import com.aaronhowser1.documentmod.json.DocumentationLoader;
+import com.aaronhowser1.documentmod.json.DocumentationRegistry;
 import com.aaronhowser1.documentmod.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Mod(
         modid = DocumentMod.MODID,
@@ -26,7 +36,6 @@ import javax.annotation.Nonnull;
                 "after:waystones;",
         clientSideOnly = true
 )
-@Mod.EventBusSubscriber(modid = DocumentMod.MODID)
 public class DocumentMod
 {
     public static final String MODID = "documentmod";
@@ -57,5 +66,36 @@ public class DocumentMod
     public void postInit(@Nonnull final FMLPostInitializationEvent event) {
         DocumentationLoader.INSTANCE.loadFromJson();
         proxy.postInit(event);
+    }
+
+    @EventHandler
+    public void loadComplete(@Nonnull final FMLLoadCompleteEvent event) {
+        proxy.loadComplete(event);
+        if (DYMMConfig.debugItemsNoEntry) {
+            // Okay, ready for the spam?
+            Loader.instance().getActiveModList().stream()
+                    .map(ModContainer::getModId)
+                    .map(modId -> ImmutablePair.of(modId, DocumentationRegistry.INSTANCE.getDocumentationForMod(modId)))
+                    .filter(pair -> !pair.getRight().isEmpty())
+                    .map(pair -> ImmutableTriple.of(
+                            pair.getLeft(),
+                            ForgeRegistries.ITEMS.getEntries().stream()
+                                    .filter(it -> it.getKey().getNamespace().equals(pair.getLeft()))
+                                    .map(Map.Entry::getKey)
+                                    .collect(Collectors.toList()),
+                            pair.getRight())
+                    )
+                    .map(triple -> ImmutablePair.of(triple.getLeft(), triple.getMiddle().stream()
+                            .map(it -> ImmutableTriple.of(
+                                    triple.getLeft(), it, triple.getRight().stream()
+                                            .filter(entry -> it.equals(entry.getReferredStack().getItem().getRegistryName()))
+                                            .findFirst()
+                            )).collect(Collectors.toList()))
+                    )
+                    .flatMap(pair -> pair.getRight().stream())
+                    .filter(triple -> !triple.getRight().isPresent())
+                    .map(triple -> ImmutablePair.of(triple.getLeft(), triple.getMiddle()))
+                    .forEach(pair -> logger.warn("Found undocumented item '" + pair.getRight() + "' within the documented mod '" + pair.getLeft() + "'"));
+        }
     }
 }
--- src/main/java/com/aaronhowser1/documentmod/proxy/CommonProxy.java
+++ src/main/java/com/aaronhowser1/documentmod/proxy/CommonProxy.java
@@ -1,6 +1,7 @@
 package com.aaronhowser1.documentmod.proxy;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -10,8 +11,8 @@ public class CommonProxy {
     public void preInit(@Nonnull final FMLPreInitializationEvent event) {}
     public void init(@Nonnull final FMLInitializationEvent event) {}
     public void postInit(@Nonnull final FMLPostInitializationEvent event) {}
+    public void loadComplete(@Nonnull final FMLLoadCompleteEvent event) {}
 
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     public boolean canTranslate(@Nonnull final String key) {
         return true; // There is no way to check on the server
     }
