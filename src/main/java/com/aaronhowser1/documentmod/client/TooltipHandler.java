5d1be4f3e97d9a3897d915cc9d43d41cc08949a9
TheSilkMiner <thesilkminer@outlook.com>
2019-08-30 10:58:03 +0200

    Fix tooltips not working on some items
    The item we are hovering does not necessarily get documented within the
    same mod that added it (e.g. tweaks to vanilla, or potions), so we need
    to account for that.
    
    Also, now NBT-filtering works properly.
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/aaronhowser1/documentmod/client/TooltipHandler.java
+++ src/main/java/com/aaronhowser1/documentmod/client/TooltipHandler.java
@@ -6,7 +6,7 @@ import com.aaronhowser1.documentmod.json.ModDocumentation;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -20,45 +20,98 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Mod.EventBusSubscriber(modid = DocumentMod.MODID, value = Side.CLIENT)
 public final class TooltipHandler {
 
-    private static final LoadingCache<ResourceLocation, List<Pair<TextFormatting, String>>> CACHE;
+    private static final LoadingCache<ResourceLocation, List<ModDocumentation>> MOD_DOCUMENTATION_CACHE;
+    private static final LoadingCache<ItemStack, List<Pair<TextFormatting, String>>> STACKS_CACHE;
 
     private TooltipHandler() {}
 
     static {
-        CACHE = CacheBuilder.newBuilder()
+        MOD_DOCUMENTATION_CACHE = CacheBuilder.newBuilder()
                 .maximumSize(500)
                 .expireAfterAccess(5, TimeUnit.MINUTES)
                 .expireAfterWrite(10, TimeUnit.MINUTES)
-                .build(new CacheLoader<ResourceLocation, List<Pair<TextFormatting, String>>>() {
+                .build(new CacheLoader<ResourceLocation, List<ModDocumentation>>() {
+                    @Nonnull
                     @Override
-                    public List<Pair<TextFormatting, String>> load(@Nonnull final ResourceLocation key) {
-                        return DocumentationRegistry.INSTANCE.getDocumentationForMod(key.getNamespace()).stream()
+                    public List<ModDocumentation> load(@Nonnull final ResourceLocation key) {
+                        return DocumentationRegistry.INSTANCE.getRegistry().getEntries().stream()
+                                .map(Map.Entry::getValue)
                                 .filter(it -> it.getReferredStacks().stream()
                                         .map(ItemStack::getItem)
                                         .map(Item::getRegistryName)
                                         .filter(Objects::nonNull)
                                         .anyMatch(key::equals)
                                 )
-                                .map(ModDocumentation::getTooltipKeys)
-                                .findFirst()
-                                .orElseGet(ImmutableList::of);
+                                .collect(Collectors.toList());
+                    }
+                });
+
+        // Stacks are the worst as keys, because they don't override equals, they rely on
+        // identity etc... For this reason, this cache is only useful in case someone is
+        // looking at the same ItemStack for a certain amount of time. Any operation on
+        // an ItemStack will make it change (especially with "copy") and so on. This is
+        // the reason why we keep them for such a low time: a few seconds basically.
+        STACKS_CACHE = CacheBuilder.newBuilder()
+                .maximumSize(50)
+                .expireAfterAccess(3, TimeUnit.SECONDS)
+                .expireAfterWrite(5, TimeUnit.SECONDS)
+                .build(new CacheLoader<ItemStack, List<Pair<TextFormatting, String>>>() {
+                    @Nonnull
+                    @Override
+                    public List<Pair<TextFormatting, String>> load(@Nonnull final ItemStack key) {
+                        final ResourceLocation registryName = key.getItem().getRegistryName();
+                        if (registryName == null) return Lists.newArrayList();
+
+                        final List<ModDocumentation> documentationCandidates = MOD_DOCUMENTATION_CACHE.getUnchecked(registryName);
+                        final List<Pair<TextFormatting, String>> lines = Lists.newArrayList();
+
+                        documentationCandidates.forEach(candidate -> {
+                            boolean matches = false;
+                            for (@Nonnull final ItemStack stack : candidate.getReferredStacks()) {
+                                if (matches) break;
+
+                                // First we check the items
+                                matches = ItemStack.areItemsEqual(stack, key);
+
+                                if (matches && stack.hasTagCompound()) {
+                                    // This item stack has a tag compound: we need to go deeper
+                                    matches = ItemStack.areItemStackTagsEqual(stack, key);
+
+                                    if (!matches) {
+                                        // We are going to match if and only if the "difference"
+                                        // between the two is just the display name, unless the
+                                        // second stack has itself a display name
+                                        if (key.hasDisplayName() && stack.hasDisplayName()) continue;
+                                        if (!key.hasDisplayName()) continue;
+
+                                        stack.setStackDisplayName(key.getDisplayName());
+
+                                        matches = ItemStack.areItemStackTagsEqual(stack, key);
+                                    }
+                                }
+                            }
+
+                            if (!matches) return;
+
+                            lines.addAll(candidate.getTooltipKeys());
+                        });
+
+                        return lines;
                     }
                 });
     }
 
     @SubscribeEvent
     public static void onItemTooltipEvent(@Nonnull final ItemTooltipEvent event) {
-        final Item item = event.getItemStack().getItem();
-        final ResourceLocation registryName = item.getRegistryName();
-        if (registryName == null) return;
-        final List<Pair<TextFormatting, String>> lines = CACHE.getUnchecked(registryName);
-        lines.forEach(it -> {
+        STACKS_CACHE.getUnchecked(event.getItemStack()).forEach(it -> {
             final String line;
             if (it.getLeft() == null) {
                 line = I18n.format(it.getRight());
