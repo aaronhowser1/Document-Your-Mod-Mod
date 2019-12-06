8c8e4a17fdc3e0bffff72d2eff786b718b2ab22d
TheSilkMiner <thesilkminer@outlook.com>
2019-12-06 14:42:01 +0100

    Disable all mod functionality
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/aaronhowser1/dym/DocumentYourMod.java
+++ src/main/java/com/aaronhowser1/dym/DocumentYourMod.java
@@ -7,21 +7,18 @@ import javax.annotation.Nonnull;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.MOD_VERSION, dependencies = Constants.MOD_DEPENDENCIES, clientSideOnly = true)
 public final class DocumentYourMod {
-
-    private final com.aaronhowser1.documentmod.DocumentMod bridge = new com.aaronhowser1.documentmod.DocumentMod();
-
     @Mod.EventHandler
     public void onConstruct(@Nonnull final FMLConstructionEvent event) {}
 
     @Mod.EventHandler
-    public void onPreInitialization(@Nonnull final FMLPreInitializationEvent event) {bridge.preInit(event);}
+    public void onPreInitialization(@Nonnull final FMLPreInitializationEvent event) {}
 
     @Mod.EventHandler
-    public void onInitialization(@Nonnull final FMLInitializationEvent event) {bridge.init(event);}
+    public void onInitialization(@Nonnull final FMLInitializationEvent event) {}
 
     @Mod.EventHandler
-    public void onPostInitialization(@Nonnull final FMLPostInitializationEvent event) {bridge.postInit(event);}
+    public void onPostInitialization(@Nonnull final FMLPostInitializationEvent event) {}
 
     @Mod.EventHandler
-    public void onLoadComplete(@Nonnull final FMLLoadCompleteEvent event) {bridge.loadComplete(event);}
+    public void onLoadComplete(@Nonnull final FMLLoadCompleteEvent event) {}
 }
