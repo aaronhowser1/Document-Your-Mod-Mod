c227cee5e83e884d577f219102e54648051e4bce
TheSilkMiner <thesilkminer@outlook.com>
2019-12-11 17:32:45 +0100

    Fix metadata identifier requiring a ResourceLocation
    They should require raw strings, not namespace-bound ones
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/aaronhowser1/dym/api/loading/metadata/MetadataListenerRegistry.java
+++ src/main/java/com/aaronhowser1/dym/api/loading/metadata/MetadataListenerRegistry.java
@@ -1,11 +1,9 @@
 package com.aaronhowser1.dym.api.loading.metadata;
 
-import net.minecraft.util.ResourceLocation;
-
 import javax.annotation.Nonnull;
 import java.util.List;
 
 public interface MetadataListenerRegistry {
-    void register(@Nonnull final ResourceLocation metadataIdentifier, @Nonnull final MetadataListener listener);
-    @Nonnull List<MetadataListener> findAllFor(@Nonnull final ResourceLocation metadataIdentifier);
+    void register(@Nonnull final String metadataIdentifier, @Nonnull final MetadataListener listener);
+    @Nonnull List<MetadataListener> findAllFor(@Nonnull final String metadataIdentifier);
 }
