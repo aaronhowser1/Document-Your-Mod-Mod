d5527a5fac26a0d803f490b46a32c89c41127585
TheSilkMiner <thesilkminer@outlook.com>
2019-12-28 14:58:20 +0100

    DYMM has ceased to exist, DYMM is no more, long live DYMM
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/aaronhowser1/documentmod/json/factory/Factory.java
+++
@@ -1,9 +0,0 @@
-package com.aaronhowser1.documentmod.json.factory;
-
-// Marker interface so that the map in DocumentationLoader is a little little bit more robust
-public interface Factory<T> {
-    @SuppressWarnings("unchecked")
-    default T asT() {
-        return (T) this;
-    }
-}
--- src/main/java/com/aaronhowser1/documentmod/utility/TriPredicate.java
+++
@@ -1,9 +0,0 @@
-package com.aaronhowser1.documentmod.utility;
-
-import javax.annotation.Nullable;
-
-@FunctionalInterface
-public interface TriPredicate<T, S, U> {
-
-    boolean test(@Nullable final T t, @Nullable final S s, @Nullable final U u);
-}
